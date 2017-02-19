950975d0f65620bb76bcff9763f0b3ea31fca2f0
Jürgen Mülbert <develop@juergen-muelbert.de>
2017-02-19 17:07:18 +0100

    Update Readme.md and Comments

--- New-OpenOrders/New-OpenOrders.psm1
+++ New-OpenOrders/New-OpenOrders.psm1
@@ -9,32 +9,32 @@
 .EXAMPLE
    Ein weiteres Beispiel für die Verwendung dieses Cmdlets
 .INPUTS
-   Eingaben in dieses Cmdlet (falls vorhanden)
+ userfile - The CSV File for Filter the Source of usernames
+ datafile - The Excel Input File
+ outputPath - The Path to write the results
 .OUTPUTS
-   Ausgabe dieses Cmdlets (falls vorhanden)
+ CSV Files are the results of the filter.
 .NOTES
-   Allgemeine Hinweise
+   The Format of the temporary excelfile change to:
+   A1 = Auftrag Nr.                     Auftragnummer
+   B1 = Hauptbereich                    Hauptbereich
+   C1 = Auftragsdatum                   Auftragsdatum
+   D1 = Tage offen                      Tage_offen
+   E1 = Deb.-Nr.                        Kundennummer
+   F1 = Deb.-Name                       Kundenname
+   G1 = VerkÃ¤ufer Serviceberater       Berater
+   H1 = Arbeitswert                     Arbeit
+   I1 = Teile                           Teile
+   J1 = Fremdleistung                   Fremdleistung
+   K1 = Andere                          Andere
+   L1 = Gesamt                          Gesamt
+   M1 = Auftragswert bereits geliefert  Geliefert
 .COMPONENT
-   Die Komponente, zu der dieses Cmdlet gehört
+   Import-Excel
 .ROLE
    Die Rolle, zu der dieses Cmdlet gehört
 .FUNCTIONALITY
    Die Funktionalität, die dieses Cmdlet am besten beschreibt
-# Create_OpenOrders.ps1
-#
-# A = Auftrag Nr.
-# B = Hauptbereich
-# C = Auftragsdatum
-# D = Tage offen
-# E = Deb.-Nr.
-# F = Deb.-Name
-# G = VerkÃ¤ufer Serviceberater
-# H = Arbeitswert
-# I = Teile
-# J = Fremdleistung
-# K = Andere
-# L = Gesamt
-# M = Auftragswert bereits geliefert
 #>
 
 function New-OpenOrders {
@@ -66,6 +66,9 @@ function New-OpenOrders {
         [string]$outputPath
     )
 
+    <#
+        Check the Input Parameters if valid
+    #>
     Begin
     {
     
@@ -93,7 +96,7 @@ function New-OpenOrders {
 
         $ws.Cells["A1"].Value = "Auftragnummer"
         $ws.Cells["B1"].Value = "Hauptbereich"
-        $ws.Cells["C1"].Value = "Auftragdatum"
+        $ws.Cells["C1"].Value = "Auftragsdatum"
         $ws.Cells["D1"].Value = "Tage_offen"
         $ws.Cells["E1"].Value = "Kundennummer"
         $ws.Cells["F1"].Value = "Kundenname"
@@ -105,9 +108,10 @@ function New-OpenOrders {
         $ws.Cells["L1"].Value = "Gesamt"
         $ws.Cells["M1"].Value = "Geliefert"
  
+        # Set Datum-Format for this Cells
         $ws.Cells["C2:C200"].Style.Numberformat.Format = "dd-mm-yy"
+        #Set Numberformat for this area 0,00
         $ws.Cells["H2:M200"].Style.Numberformat.Format = "#,##0.00"
-
 
         $ws.Cells.AutoFitColumns()
 
@@ -125,7 +129,7 @@ function New-OpenOrders {
                 $fileName = '.\' + $berater.Name + '.csv'
                 $pathAndFile = $outputPath + "\" + $fileName
 
-                $c | Select-Object 'Auftragnummer', 'Auftragdatum', 'Tage_offen', 'Kundennummer', 'Kundenname',  'Berater', 'Arbeitswert', 'Teile', 'Fremdleistung', 'Andere', 'Gesamt', 'Geliefert' | 
+                $c | Select-Object 'Auftragnummer', 'Auftragsdatum', 'Tage_offen', 'Kundennummer', 'Kundenname',  'Berater', 'Arbeitswert', 'Teile', 'Fremdleistung', 'Andere', 'Gesamt', 'Geliefert' | 
                 Where-Object { $_.'Berater' -like $berater.Match } | Export-Csv $pathAndFile      
     
             }
--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# create-openorders [powershell]
-    Create from a Collection of Data extractions for salespersons.
-    This will be saved in a extra excelfile for each salesperson.
+# New-OpenOrders [powershell]
+Create from a Collection of Data extractions for salespersons.
+This will be saved in a extra excelfile for each salesperson.
+
 ## Getting Started
  - Dependencies
     - powershell 
     - powershell [Excel-Import](https://github.com/dfinke/ImportExcel)
 
 ### Excel-Import
-    The beste way to use this is install with powershell.
-    Use the Command: `Install-Module -Name ImportExcel -Scope CurrentUser`
-
+The beste way to use this is install with powershell.
+Use the Command: `Install-Module -Name ImportExcel -Scope CurrentUser`
 
 ## Usage
- - Create-OpenOrders -usersFile berater.csv -dataFile openorders.xlsx
+ - `New-OpenOrders -usersFile berater.csv -dataFile orders.xlsx`
 
         
 ## License
@@ -20,4 +20,8 @@
 EUPL-V1.1 © [Jürgen Mülbert](https:/github.com/jmuelbert/create-openorders)
 
 [Return to top](#top)
+
+[Help](#help)
+# Help for New-OpenOrders
+
 
